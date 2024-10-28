07108ace682f38986df06d059c2bf16e4c00b942
Burke Mamlin <burke@openmrs.org>
2024-10-28 11:15:25 -0400

    Improve backup handling

--- backup-data.sh
+++ backup-data.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+
+# Get the current date in YYYYMMDD format
+DATE=$(date +%Y%m%d)
+
+# Bring down the Docker container
 docker compose down
+
+# Run the backup, adding the date to the filename
 docker compose run --rm \
   -v concept-prevalence-study:/volume \
   -v $(pwd):/backup \
-  db tar czf /backup/concept-prevalence-study-backup.tar.gz -C /volume .
+  db tar czf /backup/concept-prevalence-study-backup-$DATE.tar.gz -C /volume .
+
+# Bring the Docker container back up
 docker compose up -d
--- restore-data.sh
+++ restore-data.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
+
+# Check if a backup file is provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <backup-file>"
+  exit 1
+fi
+
+BACKUP_FILE=$1
+
+# Bring down the Docker container
 docker compose down
+
+# Run the restore, using the specified backup file
 docker compose run --rm \
   -v concept-prevalence-study:/volume \
   -v $(pwd):/backup \
-  db sh -c "tar xzf /backup/concept-prevalence-study-backup.tar.gz -C /var/lib/mysql"
+  db sh -c "tar xzf /backup/$BACKUP_FILE -C /var/lib/mysql"
+
+# Bring the Docker container back up
 docker compose up -d
